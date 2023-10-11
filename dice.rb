d2334a2e63909e94cc514c95ab6ebbb6e1bc2bd2
ntran999 <146213565+ntran999@users.noreply.github.com>
2023-10-11 14:18:25 +0000

    elephant_add

--- dice.rb
+++ dice.rb
@@ -1,11 +1,35 @@
 require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
-get("/") do
-  lucky_numb = rand(100)
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-  return "Your lucky number today is" + lucky_numb.to_s
+get ("/") do
+  erb(:elephant)
 end
 
-get("/howdy") do
-  "This is the homepage"
+get ("/dice/2/6") do
+  first_dice = rand(1..6)
+  second_dice = rand(1..6)
+  sum = first_dice + second_dice
+
+  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+   <p>#{first_dice}</p>
+   <p>#{second_dice}</p>"
+end
+
+get ("/dice/2/10") do
+  first_dice = rand(1..10)
+  second_dice = rand(1..10)
+  
+
+  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
